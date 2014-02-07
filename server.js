50c9626da80760d738a4b87fad1adb8f4861f48b
Apon Palanuwech <palaa159@newschool.edu>
2014-02-07 01:56:05 -0500

    007

--- server.js
+++ server.js
@@ -87,7 +87,8 @@ io.sockets.on('connection', function() {
 });
 
 // MONGO
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient,
+	format = require('util').format;
 
 MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
 	if (err) throw err;
